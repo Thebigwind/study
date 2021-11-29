0c322bdc657c7cc4d2bb00e1db5fe14c2e6391a4
luxuefeng <luxuefeng@xiaozhu.com>
2021-11-29 23:23:50 +0800

    测试

--- wechat/main.go
+++ wechat/main.go
@@ -7,12 +7,15 @@ func main() {
 	wxInfo, err := LoginWXSmall(code)
 	if err != nil {
 		fmt.Printf("LoginWXSmall err:%s",err.Error())
-		return
+		//return
 	}
+	fmt.Printf("wxInfo:%v",wxInfo)
+	sessionKey := "YUuU7qjh5cJ0pFZKPqdzvg=="
 
-	encryptData := ""
-	iv := ""
-	m,err := DecryptWXOpenData(wxInfo.Sessionkey,encryptData, iv)
+	encryptData := "RHJlWWdiSjVFTmVjZFZsdGRYUVNRd290UHdpUWtJem9LdlNHc2tpaUdSY3NRMmNnZmY3ZGR3U3FuUUEvU3p6d3QvNmsxbkF5bWpmZG9Zd29lMk5vaHNIUGZnelkyTHUxWWNNSjRwQzhnTFlVTlZmNVcxakptcVM5NjdDQlFFV1FQbDR6bnpmNlVpME11UmVua05OTDE2dHF5Qnk4TkVQa1VkYmgxbGgwengrQS8vYTJwL0R1UytBMmJ6eXZzQ3MwUE5jMmRNcDBxbTlvTWVzdVhGaDVlZz09"
+	//encryptData := "6Ebfb0UsrK2DwXP2VaKnvcpW4m/d1AeNclsjYg+JNZylJLwbk0VvmkugidjVVE+Kc0siY2qNZhxRU21iMRi7HudWDbhdDd8nHUHzljjowPokd4uF7KL1eSQPNXb21LanVZLXjZQw6RfRN39tIn/bnQe2/2Yn1jxRPNf1+tmQZ/2pboJDgdCY620BYo9yD5eI5Ot9Opveov+aslSoxFttKw=="
+	iv := "nG9z27AERfB8jKuH8NNLhg==" // bGpacFpyc1NIQ2EraTZkbDZlQW9WZz09
+	m,err := DecryptWXOpenData(sessionKey,encryptData, iv)
 	if err != nil{
 		fmt.Printf("DecryptWXOpenData err:%s",err.Error())
 		return
